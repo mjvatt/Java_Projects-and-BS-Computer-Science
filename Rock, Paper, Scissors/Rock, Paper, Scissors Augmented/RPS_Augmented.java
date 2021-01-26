a6efdad5276c04dcb330a6bfe654137f24aaa9ad
mjvatt <56173397+mjvatt@users.noreply.github.com>
2021-01-26 18:42:56 -0500

    Update RPS_Augmented.java

--- Rock, Paper, Scissors/Rock, Paper, Scissors Augmented/RPS_Augmented.java
+++ Rock, Paper, Scissors/Rock, Paper, Scissors Augmented/RPS_Augmented.java
@@ -2,6 +2,7 @@ package git_hub_projects;
 
 public class RPS_Augmented {
 	
+	// Start the game and then check for the outcome
 	public static void main(String[] args) {
 		
 		Question question = new MultipleChoiceQuestion(
